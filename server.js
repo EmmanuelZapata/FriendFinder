933e85f16e5486144d9f99121dd05a14f9a5145f
EmmanuelZapata <zapata.emmanuel@gmail.com>
2019-06-10 17:06:17 -0500

    trying to fix survey.html

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-// What we neeed!
+// Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 
 // Sets up the Express App
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-// Using body-parser to be able to read json in express 
+// For serving of static CSS
+app.use(express.static(__dirname + "/app/css"));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Starts the server to begin listening
+// API and HTML routes
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+	console.log("App listening on PORT: " + PORT);
 });
-
-// Requires and sets the HTML routes in this file
-require('./app/routing/htmlRoutes.js')(app);
-
-// Requires and sets the API routes in this file
-require('./app/routing/apiRoutes.js')(app);

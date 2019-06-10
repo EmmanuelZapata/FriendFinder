8bc8381ba56a14e917b70b4270b0dcfb594689a9
EmmanuelZapata <zapata.emmanuel@gmail.com>
2019-06-10 17:48:32 -0500

    found this way of doing it instead

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,13 +4,12 @@ var path = require("path");
 
 // Exports the variable
 module.exports = function(app) {
-    // Sets the route for the home page
-    app.get('/', function(req, res) {
-        res.sendFile(path.join(__dirname, '../public/home.html'));
-    });
-
-    // Sets the route for the about page
-    app.get('/survey', function(req, res) {
-        res.sendFile(path.join(__dirname, '../public/survey.html'));
-    });
-}
+   
+    app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+        });
+        
+    app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+        });
+    };

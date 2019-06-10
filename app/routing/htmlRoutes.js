910e0f3bf1085445afab76b27a3e60ca98fd7d22
EmmanuelZapata <zapata.emmanuel@gmail.com>
2019-06-10 17:30:32 -0500

    fun times continue.... maybe a post

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -13,4 +13,7 @@ module.exports = function(app) {
     app.get('/survey', function(req, res) {
         res.sendFile(path.join(__dirname, '../public/survey.html'));
     });
+
+    app.post('/api/friends', function(req, res) {});
+
 }
